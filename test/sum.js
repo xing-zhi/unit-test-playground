94b73b382d2351f228363f5567e25435091d0603
xingzhi <xing_zhi@126.com>
2015-11-18 09:35:44 +0800

    update test

--- test/sum.js
+++ test/sum.js
@@ -7,6 +7,7 @@ const sum = require('../js/sum');
 test('test for function sum', function(t) {
   t.equal(sum(1, 2, 3, 4), 10);
   t.equal(sum(-1, -2, -3, -4), -10);
+  t.equal(sum(-1, 0, 1), 0);
 
   t.end();
 });
